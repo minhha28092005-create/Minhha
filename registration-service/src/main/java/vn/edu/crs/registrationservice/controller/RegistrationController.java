a52a44f31d4c97498afcb838ba495f569842dd46
HA NGUYEN MINH <2311062555@hunre.edu.vn>
2026-09-06 02:41:03 +0700

    chore: final integration review for v1.0

--- registration-service/src/main/java/vn/edu/crs/registrationservice/controller/RegistrationController.java
+++ registration-service/src/main/java/vn/edu/crs/registrationservice/controller/RegistrationController.java
@@ -4,14 +4,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import vn.edu.crs.registrationservice.dto.RegistrationRequestDTO;
 import vn.edu.crs.registrationservice.entity.Registration;
 import vn.edu.crs.registrationservice.service.RegistrationService;
@@ -34,7 +28,6 @@ public class RegistrationController {
     }
 
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void cancel(
             @PathVariable Long id
     ) {
